37ce78269d4f830c8c8afe6582f14b30777d6de2
Шантажная пена <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2024-05-21 18:26:57 +0300

    Update and rename struct.sql to CreateTables.sql
    Создание объяснения в запросе.

---
+++ db/CreateTables.sql
@@ -0,0 +1,57 @@
+/*
+ *   В запросе существуют следуюшие связи:
+ *       [groups].ID (PK) -> [users].GID (FK)
+ *       [users].ID (PK) -> [messages].UID (FK)
+ *       [types].ID (PK) -> [messages].TID (FK)
+ */
+
+-- Внешние ключи называют ограничениями, потому что
+-- невозможно добавить в таблицу с внешним ключом (ссылкой на первичный ключ)
+-- значение, которого не существует в таблице с первичным ключом.
+--
+-- Если в [groups] нет группы 3, внести пользователя с этой группой в [users] невозможно
+-- 
+-- Создание связей написано в запросе несколько раз. Связи комментариями еще раз указаны выше
+
+-- СОЗДАНИЕ СЛОЯ УЧЕТНЫХ ЗАПИСЕЙ --
+CREATE TABLE [groups]
+(
+    [id] int not null
+        PRIMARY KEY, 
+    [title] NVARCHAR(MAX),
+    [content] NVARCHAR(MAX)
+)
+
+CREATE TABLE [users]
+(
+    [id] INT NOT NULL
+        PRIMARY KEY,
+
+    
+    [name] NVARCHAR(MAX),
+    [sname] NVARCHAR(MAX),
+    [phone] NVARCHAR(MAX),
+    [gid] INT NOT NULL 
+        FOREIGN KEY REFERENCES [groups](id)
+)
+
+-- СОЗДАНИЕ СЛОЯ СООБЩЕНИЙ --
+CREATE TABLE [types]
+(
+    [id] INT NOT NULL
+        PRIMARY KEY,
+    [title] NVARCHAR(MAX),
+    [content] NVARCHAR(MAX)
+)
+
+CREATE TABLE [messages]
+(
+    [id] INT NOT NULL,
+    [uid] INT NOT NULL
+        FOREIGN KEY REFERENCES [users](id),
+    [tid] INT NOT NULL
+        FOREIGN KEY REFERENCES [types](id),
+    [title] NVARCHAR(MAX),
+    [content] NVARCHAR(MAX),
+    [time] NVARCHAR(MAX)
+)
